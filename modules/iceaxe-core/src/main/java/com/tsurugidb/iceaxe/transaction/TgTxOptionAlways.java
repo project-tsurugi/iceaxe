85d163d93f5246fdd96c571436a226e6d919ff0d
Hishida Masato <hishida@nautilus-technologies.com>
2022-08-23 10:33:53 +0900

    Modified LOG.trace

--- modules/iceaxe-core/src/main/java/com/tsurugidb/iceaxe/transaction/TgTxOptionAlways.java
+++ modules/iceaxe-core/src/main/java/com/tsurugidb/iceaxe/transaction/TgTxOptionAlways.java
@@ -2,6 +2,9 @@ package com.tsurugidb.iceaxe.transaction;
 
 import javax.annotation.concurrent.ThreadSafe;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.tsurugidb.iceaxe.transaction.exception.TsurugiTransactionException;
 
 /**
@@ -9,6 +12,7 @@ import com.tsurugidb.iceaxe.transaction.exception.TsurugiTransactionException;
  */
 @ThreadSafe
 public class TgTxOptionAlways implements TgTxOptionSupplier {
+    private static final Logger LOG = LoggerFactory.getLogger(TgTxOptionAlways.class);
 
     /**
      * create TgTxOptionAlways
@@ -46,11 +50,14 @@ public class TgTxOptionAlways implements TgTxOptionSupplier {
 
         if (isRetryable(e)) {
             if (attempt < attemtMaxCount) {
+                LOG.trace("tx retry. e={}", e.getMessage());
                 return TgTxState.execute(transactionOption);
             }
+            LOG.trace("tx retry over. e={}", e.getMessage());
             return TgTxState.retryOver();
         }
 
+        LOG.trace("tx not retryable. e={}", e.getMessage());
         return TgTxState.notRetryable();
     }
 }
--- modules/iceaxe-core/src/main/java/com/tsurugidb/iceaxe/transaction/TgTxOptionList.java
+++ modules/iceaxe-core/src/main/java/com/tsurugidb/iceaxe/transaction/TgTxOptionList.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import javax.annotation.concurrent.ThreadSafe;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.tsurugidb.iceaxe.transaction.exception.TsurugiTransactionException;
 
 /**
@@ -11,6 +14,7 @@ import com.tsurugidb.iceaxe.transaction.exception.TsurugiTransactionException;
  */
 @ThreadSafe
 public class TgTxOptionList implements TgTxOptionSupplier {
+    private static final Logger LOG = LoggerFactory.getLogger(TgTxOptionList.class);
 
     /**
      * create TgTransactionOptionList
@@ -57,11 +61,14 @@ public class TgTxOptionList implements TgTxOptionSupplier {
 
         if (isRetryable(e)) {
             if (attempt < transactionOptionList.size()) {
+                LOG.trace("tx retry. e={}", e.getMessage());
                 return TgTxState.execute(transactionOptionList.get(attempt));
             }
+            LOG.trace("tx retry over. e={}", e.getMessage());
             return TgTxState.retryOver();
         }
 
+        LOG.trace("tx not retryable. e={}", e.getMessage());
         return TgTxState.notRetryable();
     }
 }
