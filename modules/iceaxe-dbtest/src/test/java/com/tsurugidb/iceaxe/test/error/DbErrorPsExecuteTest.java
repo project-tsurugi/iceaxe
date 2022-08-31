3d0db544c8a1a5e7c17cd23c6338f217c494c2bb
Hishida Masato <hishida@nautilus-technologies.com>
2022-08-31 10:41:47 +0900

    DbErrorPsExecuteTest: updated

--- modules/iceaxe-dbtest/src/test/java/com/tsurugidb/iceaxe/test/error/DbErrorPsExecuteTest.java
+++ modules/iceaxe-dbtest/src/test/java/com/tsurugidb/iceaxe/test/error/DbErrorPsExecuteTest.java
@@ -1,21 +1,28 @@
 package com.tsurugidb.iceaxe.test.error;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.io.IOException;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
 
+import com.nautilus_technologies.tsubakuro.exception.SqlServiceCode;
+import com.tsurugidb.iceaxe.exception.TsurugiIOException;
+import com.tsurugidb.iceaxe.statement.TgParameterList;
+import com.tsurugidb.iceaxe.statement.TgParameterMapping;
+import com.tsurugidb.iceaxe.statement.TgVariable;
 import com.tsurugidb.iceaxe.test.util.DbTestTableTester;
-import com.tsurugidb.iceaxe.test.util.TestEntity;
+import com.tsurugidb.iceaxe.transaction.exception.TsurugiTransactionIOException;
 
 /**
  * PreparedStatement execute error test
  */
 class DbErrorPsExecuteTest extends DbTestTableTester {
+
+    private static final int SIZE = 4;
 
     @BeforeEach
     void beforeEach(TestInfo info) throws IOException {
@@ -23,41 +30,86 @@ class DbErrorPsExecuteTest extends DbTestTableTester {
 
         dropTestTable();
         createTestTable();
+        insertTestTable(SIZE);
 
         LOG.debug("{} end start", info.getDisplayName());
     }
 
     @Test
-    @Disabled // TODO remove Disabled
     void intsertByExecuteQuery() throws IOException {
-        var entity = new TestEntity(123, 456, "abc");
-
         var sql = "insert into " + TEST //
                 + "(" + TEST_COLUMNS + ")" //
-                + "values(" + entity.getFoo() + ", " + entity.getBar() + ", '" + entity.getZzz() + "')";
+                + "values(123, 456, 'abc')";
 
         var session = getSession();
         var tm = createTransactionManagerOcc(session);
         try (var ps = session.createPreparedQuery(sql)) {
-            ps.executeAndGetList(tm);
+            var e = assertThrows(TsurugiTransactionIOException.class, () -> {
+                ps.executeAndGetList(tm);
+            });
+            assertEqualsCode(SqlServiceCode.ERR_ILLEGAL_OPERATION, e);
         }
 
-        assertEqualsTestTable(entity);
+        assertEqualsTestTable(SIZE);
     }
 
     @Test
-    @Disabled // TODO remove Disabled
     void selectByExecuteStatement() throws IOException {
-        int size = 4;
-        insertTestTable(size);
-
         var sql = "select * from " + TEST;
 
         var session = getSession();
         var tm = createTransactionManagerOcc(session);
         try (var ps = session.createPreparedStatement(sql)) {
             int count = ps.executeAndGetCount(tm);
-            assertEquals(size, count);
+            assertEquals(-1, count); // TODO 0?
         }
+    }
+
+    @Test
+    void insertParameterSizeUnmatch() throws IOException {
+        var foo = TgVariable.ofInt4("foo");
+        var bar = TgVariable.ofInt8("bar");
+        var zzz = TgVariable.ofCharacter("zzz");
+        var sql = "insert into " + TEST //
+                + "(" + TEST_COLUMNS + ")" //
+                + "values(" + foo + ", " + bar + ", " + zzz + ")";
+        var parameterMapping = TgParameterMapping.of(foo, bar, zzz);
+
+        var session = getSession();
+        var tm = createTransactionManagerOcc(session);
+        try (var ps = session.createPreparedStatement(sql, parameterMapping)) {
+            var e = assertThrows(TsurugiTransactionIOException.class, () -> {
+                var plist = TgParameterList.of(foo.bind(123), bar.bind(456) /* ,zzz */);
+                ps.executeAndGetCount(tm, plist);
+            });
+            assertEqualsCode(SqlServiceCode.ERR_UNRESOLVED_HOST_VARIABLE, e);
+            // TODO エラー詳細情報の確認
+        }
+
+        assertEqualsTestTable(SIZE);
+    }
+
+    @Test
+    void insertParameterTypeUnmatch() throws IOException {
+        var foo = TgVariable.ofInt8("foo"); // INT4
+        var bar = TgVariable.ofInt4("bar"); // INT8
+        var zzz = TgVariable.ofInt4("zzz"); // CHARACTER
+        var sql = "insert into " + TEST //
+                + "(" + TEST_COLUMNS + ")" //
+                + "values(" + foo + ", " + bar + ", " + zzz + ")";
+        var parameterMapping = TgParameterMapping.of(foo, bar, zzz);
+
+        var session = getSession();
+        var tm = createTransactionManagerOcc(session);
+        try (var ps = session.createPreparedStatement(sql, parameterMapping)) {
+            var e = assertThrows(TsurugiIOException.class, () -> {
+                var plist = TgParameterList.of(foo.bind(123), bar.bind(456), zzz.bind(789));
+                ps.executeAndGetCount(tm, plist);
+            });
+            assertEqualsCode(SqlServiceCode.ERR_COMPILER_ERROR, e);
+            // TODO エラー詳細情報の確認
+        }
+
+        assertEqualsTestTable(SIZE);
     }
 }
