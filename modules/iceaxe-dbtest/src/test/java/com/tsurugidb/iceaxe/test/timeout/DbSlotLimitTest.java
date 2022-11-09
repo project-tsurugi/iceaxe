15d537ab443b721c44b46a763cf1f0a1017438a4
Hishida Masato <hishida@nautilus-technologies.com>
2022-11-09 13:50:39 +0900

    DbSlotLimitTest: disabled

--- modules/iceaxe-dbtest/src/test/java/com/tsurugidb/iceaxe/test/timeout/DbSlotLimitTest.java
+++ modules/iceaxe-dbtest/src/test/java/com/tsurugidb/iceaxe/test/timeout/DbSlotLimitTest.java
@@ -4,18 +4,28 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
 
+import com.tsurugidb.iceaxe.result.TsurugiResultEntity;
+import com.tsurugidb.iceaxe.result.TsurugiResultSet;
 import com.tsurugidb.iceaxe.session.TsurugiSession;
+import com.tsurugidb.iceaxe.statement.TsurugiPreparedStatementQuery0;
 import com.tsurugidb.iceaxe.transaction.TgTxOption;
+import com.tsurugidb.iceaxe.transaction.TsurugiTransaction;
+import com.tsurugidb.iceaxe.transaction.exception.TsurugiTransactionException;
 import com.tsurugidb.tsubakuro.channel.common.connection.wire.impl.ResponseBox;
 
 /**
  * slot limit test
  */
+@Disabled // TODO remove Disabled
 public class DbSlotLimitTest extends DbTimetoutTest {
 
     @BeforeEach
@@ -43,24 +53,40 @@ public class DbSlotLimitTest extends DbTimetoutTest {
             transaction.getLowTransaction();
 
             try (var ps = session.createPreparedQuery(SELECT_SQL)) {
-                pipeServer.setPipeWrite(false);
+                var rsList = new ArrayList<TsurugiResultSet<?>>();
 
+                pipeServer.setPipeWrite(false);
                 try {
-                    int responseBoxSize = ResponseBox.responseBoxSize();
-                    for (int i = 0; i < responseBoxSize + 1; i++) {
-                        try {
-                            ps.execute(transaction);
-                        } catch (IOException e) {
-                            assertEquals("no available response box", e.getMessage());
-                            assertEquals(responseBoxSize, i);
-                            return;
-                        }
-                    }
-                    fail("slot limit over did not occur");
+                    execute(transaction, ps, rsList);
                 } finally {
                     pipeServer.setPipeWrite(true);
                 }
+
+                int i = 0;
+                for (var rs : rsList) {
+                    LOG.trace("close i={}", i);
+                    rs.close(); // FIXME Tsubakuroのタイムアウトが未実装なので、ここで待ちに入る
+                    i++;
+                }
             }
         }
+    }
+
+    private void execute(TsurugiTransaction transaction, TsurugiPreparedStatementQuery0<TsurugiResultEntity> ps, List<TsurugiResultSet<?>> rsList) throws TsurugiTransactionException {
+        int responseBoxSize = ResponseBox.responseBoxSize();
+        for (int i = 0; i < responseBoxSize + 1; i++) {
+            LOG.trace("i={}", i);
+            try {
+                var rs = ps.execute(transaction);
+                rs.setRsConnectTimeout(1, TimeUnit.MILLISECONDS);
+                rs.setRsCloseTimeout(1, TimeUnit.MILLISECONDS);
+                rsList.add(rs);
+            } catch (IOException e) {
+                assertEquals("no available response box", e.getMessage());
+                assertEquals(responseBoxSize, i);
+                return;
+            }
+        }
+        fail("slot limit over did not occur");
     }
 }
