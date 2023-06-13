a1d790b0f52d2e3e15c3d732ebcb66f41889049c
Hishida Masato <hishida@nautilus-technologies.com>
2023-06-13 17:27:27 +0900

    Added Example04TmRetryPredicate

---
+++ modules/iceaxe-examples/src/main/java/com/tsurugidb/iceaxe/example/Example04TmRetryPredicate.java
@@ -0,0 +1,29 @@
+package com.tsurugidb.iceaxe.example;
+
+import com.tsurugidb.iceaxe.transaction.exception.TsurugiTransactionException;
+import com.tsurugidb.iceaxe.transaction.manager.TgTmSetting;
+import com.tsurugidb.iceaxe.transaction.manager.retry.TsurugiDefaultRetryPredicate;
+import com.tsurugidb.iceaxe.transaction.manager.retry.TsurugiTmRetryPredicate;
+import com.tsurugidb.tsubakuro.sql.SqlServiceCode;
+
+/**
+ * {@link TsurugiTmRetryPredicate} example.
+ *
+ * @see Example04TmSetting
+ */
+public class Example04TmRetryPredicate {
+
+    void predicate(TgTmSetting setting) {
+        var supplier = setting.getTransactionOptionSupplier();
+        supplier.setRetryPredicate(new TsurugiDefaultRetryPredicate() {
+            @Override
+            protected boolean isAbortedRetryable(TsurugiTransactionException e) {
+                var code = e.getDiagnosticCode();
+                if (code == SqlServiceCode.ERR_ABORTED) {
+                    return true;
+                }
+                return super.isAbortedRetryable(e);
+            }
+        });
+    }
+}
