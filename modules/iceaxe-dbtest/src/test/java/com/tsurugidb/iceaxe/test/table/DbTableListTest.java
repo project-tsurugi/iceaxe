61ca416faa81efc852f11897b2d6abcd99a6a56c
Hishida Masato <hishida@nautilus-technologies.com>
2023-07-07 18:16:02 +0900

    DbTableListTest: remove Disabled

--- modules/iceaxe-dbtest/src/test/java/com/tsurugidb/iceaxe/test/table/DbTableListTest.java
+++ modules/iceaxe-dbtest/src/test/java/com/tsurugidb/iceaxe/test/table/DbTableListTest.java
@@ -1,11 +1,11 @@
 package com.tsurugidb.iceaxe.test.table;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
 import org.slf4j.LoggerFactory;
@@ -15,8 +15,9 @@ import com.tsurugidb.iceaxe.test.util.DbTestTableTester;
 /**
  * table list test
  */
-@Disabled // TODO remove Disabled. listTables()
 class DbTableListTest extends DbTestTableTester {
+
+    private static final String TEST2 = "test2";
 
     @BeforeAll
     static void beforeAll(TestInfo info) throws Exception {
@@ -24,7 +25,7 @@ class DbTableListTest extends DbTestTableTester {
         logInitStart(LOG, info);
 
         dropTestTable();
-        createTestTable();
+        dropTable(TEST2);
 
         logInitEnd(LOG, info);
     }
@@ -32,8 +33,24 @@ class DbTableListTest extends DbTestTableTester {
     @Test
     void getTableNameList() throws Exception {
         var session = getSession();
-        List<String> tableList = session.getTableNameList();
-        System.out.println(tableList);
-        assertTrue(tableList.contains(TEST));
+        {
+            List<String> tableList = session.getTableNameList();
+            assertFalse(tableList.contains(TEST));
+            assertFalse(tableList.contains(TEST2));
+        }
+        {
+            createTestTable();
+
+            List<String> tableList = session.getTableNameList();
+            assertTrue(tableList.contains(TEST));
+            assertFalse(tableList.contains(TEST2));
+        }
+        {
+            executeDdl(session, CREATE_TEST_SQL.replace(TEST, TEST2));
+
+            List<String> tableList = session.getTableNameList();
+            assertTrue(tableList.contains(TEST));
+            assertTrue(tableList.contains(TEST2));
+        }
     }
 }
