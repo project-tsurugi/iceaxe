37ade0515d4d195670e04ce4e8cb421498a88b1d
Hishida Masato <hishida@nautilus-technologies.com>
2023-07-07 09:47:30 +0900

    Added TsurugiIOExceptionTest

---
+++ modules/iceaxe-core/src/test/java/com/tsurugidb/iceaxe/exception/TsurugiIOExceptionTest.java
@@ -0,0 +1,40 @@
+package com.tsurugidb.iceaxe.exception;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.Test;
+
+class TsurugiIOExceptionTest {
+
+    @Test
+    void constructMessageIOException() {
+        var se = new IceaxeServerExceptionTestMock("test", IceaxeErrorCode.TX_LOW_ERROR);
+        var target = new TsurugiIOException("test1", new IOException(se.getMessage(), se));
+        assertEquals("test1", target.getMessage());
+        assertEquals(IceaxeErrorCode.TX_LOW_ERROR, target.getDiagnosticCode());
+    }
+
+    @Test
+    void constructServerException() {
+        var target = new TsurugiIOException(new IceaxeServerExceptionTestMock("test", IceaxeErrorCode.TX_LOW_ERROR));
+        assertEquals(IceaxeErrorCode.TX_LOW_ERROR.name() + ": test", target.getMessage());
+        assertEquals(IceaxeErrorCode.TX_LOW_ERROR, target.getDiagnosticCode());
+    }
+
+    @Test
+    void constructCode() {
+        var target = new TsurugiIOException(IceaxeErrorCode.TX_LOW_ERROR);
+        assertEquals(IceaxeErrorCode.TX_LOW_ERROR.getMessage(), target.getMessage());
+        assertEquals(IceaxeErrorCode.TX_LOW_ERROR, target.getDiagnosticCode());
+    }
+
+    @Test
+    void constructCodeIOException() {
+        var se = new IceaxeServerExceptionTestMock("test", IceaxeErrorCode.TX_ALREADY_CLOSED);
+        var target = new TsurugiIOException(IceaxeErrorCode.TX_LOW_ERROR, new IOException(se.getMessage(), se));
+        assertEquals(IceaxeErrorCode.TX_LOW_ERROR.getMessage(), target.getMessage());
+        assertEquals(IceaxeErrorCode.TX_LOW_ERROR, target.getDiagnosticCode());
+    }
+}
