5498ca5673da8d7dc36212d198c78b925d3b0f83
Hishida Masato <hishida@nautilus-technologies.com>
2023-08-09 09:28:53 +0900

    DbConnectErrorTest: Added recvIllegalMessage()

--- modules/iceaxe-dbtest/src/test/java/com/tsurugidb/iceaxe/test/connector/DbConnectErrorTest.java
+++ modules/iceaxe-dbtest/src/test/java/com/tsurugidb/iceaxe/test/connector/DbConnectErrorTest.java
@@ -7,16 +7,19 @@ import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.net.ServerSocket;
 import java.net.URI;
+import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.opentest4j.AssertionFailedError;
 
 import com.tsurugidb.iceaxe.TsurugiConnector;
 import com.tsurugidb.iceaxe.session.TgSessionOption;
 import com.tsurugidb.iceaxe.session.TgSessionOption.TgTimeoutKey;
+import com.tsurugidb.iceaxe.test.util.DbTestConnector;
 import com.tsurugidb.iceaxe.test.util.DbTestTableTester;
-import com.tsurugidb.iceaxe.transaction.option.TgTxOption;
+import com.tsurugidb.tsubakuro.channel.stream.StreamLink;
 
 /**
  * connect error test
@@ -40,17 +43,6 @@ public class DbConnectErrorTest extends DbTestTableTester {
         }
     }
 
-    private void connect(int port) throws Exception {
-        var endpoint = URI.create("tcp://localhost:" + port);
-        var connector = TsurugiConnector.of(endpoint);
-
-        var sessionOption = TgSessionOption.of();
-        sessionOption.setTimeout(TgTimeoutKey.DEFAULT, 3, TimeUnit.SECONDS);
-        try (var session = connector.createSession(sessionOption); //
-                var transaction = session.createTransaction(TgTxOption.ofOCC())) {
-        }
-    }
-
     private static class ServerThtread extends Thread {
 
         private final ServerSocket server;
@@ -69,6 +61,80 @@ public class DbConnectErrorTest extends DbTestTableTester {
                 throw new RuntimeException(e);
             } catch (IOException e) {
                 throw new UncheckedIOException(e);
+            }
+        }
+    }
+
+    @Test
+    @Disabled // TODO remove Disabled
+    void recvIllegalMessage() throws Exception {
+        try (var server = new ServerSocket(0)) {
+            int port = server.getLocalPort();
+
+            var thread = new IllegalMessageServerThtread(server);
+            thread.start();
+
+            var e = assertThrowsExactly(IOException.class, () -> connect(port));
+            try {
+                assertEquals("Server crashed", e.getMessage());
+            } catch (AssertionFailedError t) {
+                throw e;
+            }
+        }
+    }
+
+    private static class IllegalMessageServerThtread extends Thread {
+
+        private final ServerSocket server;
+
+        public IllegalMessageServerThtread(ServerSocket server) {
+            this.server = server;
+        }
+
+        @Override
+        public void run() {
+            try (var socket = server.accept()) {
+                try (var os = socket.getOutputStream()) {
+                    // 不正なメッセージを返す
+                    var buf = new byte[128];
+                    Arrays.fill(buf, (byte) 0xff);
+                    buf[0] = StreamLink.RESPONSE_RESULT_SET_PAYLOAD;
+                    os.write(buf);
+                    os.flush();
+                    TimeUnit.SECONDS.sleep(1);
+                }
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                throw new UncheckedIOException(e);
+            }
+        }
+    }
+
+    private void connect(int port) throws Exception {
+        var endpoint = URI.create("tcp://localhost:" + port);
+        var connector = TsurugiConnector.of(endpoint);
+
+        var sessionOption = TgSessionOption.of();
+        sessionOption.setTimeout(TgTimeoutKey.DEFAULT, 3, TimeUnit.SECONDS);
+        try (var session = connector.createSession(sessionOption)) {
+            session.getLowSession();
+        }
+    }
+
+    @Test
+    void sendIllegalMessage() throws Exception {
+        try (var socket = DbTestConnector.createSocket()) {
+            try (var os = socket.getOutputStream();) {
+                // 不正なメッセージを送信する
+                var buf = new byte[128];
+                Arrays.fill(buf, (byte) 0xff);
+                os.write(buf);
+                os.flush();
+                try (var is = socket.getInputStream()) {
+                    int len = is.read(buf);
+                    assertEquals(-1, len);
+                }
             }
         }
     }
