a3015d408322c7f96a53e6592c4f3f078787e95c
Hishida Masato <hishida@nautilus-technologies.com>
2023-08-28 11:29:00 +0900

    DbSelectUpdateTest: Added selectUpdate_ltx_ltx2()

--- modules/iceaxe-dbtest/src/test/java/com/tsurugidb/iceaxe/test/update/DbSelectUpdateTest.java
+++ modules/iceaxe-dbtest/src/test/java/com/tsurugidb/iceaxe/test/update/DbSelectUpdateTest.java
@@ -1,11 +1,8 @@
 package com.tsurugidb.iceaxe.test.update;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertInstanceOf;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
-
-import java.util.concurrent.ExecutionException;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -125,12 +122,101 @@ class DbSelectUpdateTest extends DbTestTableTester {
                     assertFalse(future2.isDone());
                     tx1.commit(TgCommitType.DEFAULT);
 
-                    var e = assertThrows(ExecutionException.class, () -> future2.get());
-                    var c = e.getCause();
-                    assertInstanceOf(TsurugiTransactionException.class, c);
-                    assertEqualsCode(SqlServiceCode.ERR_SERIALIZATION_FAILURE, c);
+                    future2.get();
                 }
             }
+        }
+
+        var list = selectAllFromTest();
+        int i = 0;
+        for (var entity : list) {
+            var expected = createTestEntity(i++);
+            if (expected.getFoo() == key) {
+                expected.setBar(9L);
+            }
+            assertEquals(expected, entity);
+        }
+    }
+
+    @Test
+    void selectUpdate_ltx_ltx2_seq() throws Exception {
+        TgTxOption txOption1 = TgTxOption.ofLTX(TEST);
+        TgTxOption txOption2 = TgTxOption.ofLTX(TEST);
+
+        int key = 2;
+        var selectSql = SELECT_SQL + " where foo=" + key;
+        var updateSql = "update " + TEST + " set bar=:bar where foo=" + key;
+        var updateMapping = TgParameterMapping.ofSingle("bar", long.class);
+
+        var session = getSession();
+
+        try (var selectPs = session.createQuery(selectSql, SELECT_MAPPING); //
+                var updatePs = session.createStatement(updateSql, updateMapping)) {
+            try (var tx1 = session.createTransaction(txOption1.label("tx1"))) {
+                var entity1 = tx1.executeAndGetList(selectPs).get(0);
+                tx1.executeAndGetCount(updatePs, entity1.getBar() + 1);
+                tx1.commit(TgCommitType.DEFAULT);
+            }
+            try (var tx2 = session.createTransaction(txOption2.label("tx2"))) {
+                var entity2 = tx2.executeAndGetList(selectPs).get(0);
+                tx2.executeAndGetCount(updatePs, entity2.getBar() + 9);
+                tx2.commit(TgCommitType.DEFAULT);
+            }
+        }
+
+        var list = selectAllFromTest();
+        int i = 0;
+        for (var entity : list) {
+            var expected = createTestEntity(i++);
+            if (expected.getFoo() == key) {
+                expected.setBar(expected.getBar() + 1 + 9);
+            }
+            assertEquals(expected, entity);
+        }
+    }
+
+    @Test
+    void selectUpdate_ltx_ltx2() throws Exception {
+        TgTxOption txOption1 = TgTxOption.ofLTX(TEST);
+        TgTxOption txOption2 = TgTxOption.ofLTX(TEST);
+
+        int key = 2;
+        var selectSql = SELECT_SQL + " where foo=" + key;
+        var updateSql = "update " + TEST + " set bar=:bar where foo=" + key;
+        var updateMapping = TgParameterMapping.ofSingle("bar", long.class);
+
+        var session = getSession();
+
+        try (var selectPs = session.createQuery(selectSql, SELECT_MAPPING); //
+                var updatePs = session.createStatement(updateSql, updateMapping)) {
+            try (var tx1 = session.createTransaction(txOption1.label("tx1"))) {
+                var entity1 = tx1.executeAndGetList(selectPs).get(0);
+
+                try (var tx2 = session.createTransaction(txOption2.label("tx2"))) {
+                    var entity2 = tx2.executeAndGetList(selectPs).get(0);
+                    tx2.executeAndGetCount(updatePs, entity2.getBar() + 9);
+                    var future2 = executeFuture(() -> {
+                        tx2.commit(TgCommitType.DEFAULT);
+                        return null;
+                    });
+
+                    tx1.executeAndGetCount(updatePs, entity1.getBar() + 1);
+                    assertFalse(future2.isDone());
+                    tx1.commit(TgCommitType.DEFAULT);
+
+                    future2.get(); // TODO シリアライゼーションエラーになるべき？
+                }
+            }
+        }
+
+        var list = selectAllFromTest();
+        int i = 0;
+        for (var entity : list) {
+            var expected = createTestEntity(i++);
+            if (expected.getFoo() == key) {
+                expected.setBar(expected.getBar() + 1 + 9);
+            }
+            assertEquals(expected, entity);
         }
     }
 }
