28488723908dbaaf1047178da884fcc0e3aa1985
Hishida Masato <hishida@nautilus-technologies.com>
2023-09-07 13:43:38 +0900

    DbSelectCountTest: Added countWhileInsertOcc()

--- modules/iceaxe-dbtest/src/test/java/com/tsurugidb/iceaxe/test/select/DbSelectCountTest.java
+++ modules/iceaxe-dbtest/src/test/java/com/tsurugidb/iceaxe/test/select/DbSelectCountTest.java
@@ -1,17 +1,27 @@
 package com.tsurugidb.iceaxe.test.select;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import java.io.IOException;
+import java.text.MessageFormat;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import com.tsurugidb.iceaxe.sql.TgDataType;
+import com.tsurugidb.iceaxe.sql.parameter.TgParameterMapping;
 import com.tsurugidb.iceaxe.sql.result.TgResultMapping;
+import com.tsurugidb.iceaxe.test.util.DbTestConnector;
 import com.tsurugidb.iceaxe.test.util.DbTestTableTester;
+import com.tsurugidb.iceaxe.transaction.function.TsurugiTransactionAction;
+import com.tsurugidb.iceaxe.transaction.manager.TgTmSetting;
+import com.tsurugidb.iceaxe.transaction.option.TgTxOption;
 
 /**
  * select count test
@@ -56,6 +66,97 @@ class DbSelectCountTest extends DbTestTableTester {
         try (var ps = session.createQuery(sql, resultMapping)) {
             int count = tm.executeAndFindRecord(ps).get();
             assertEquals(size, count);
+        }
+    }
+
+    @Test
+    @Disabled // TODO remove Disabled. countがinsert/commit件数と一致しないことがある
+    void countWhileInsertOcc() throws Exception {
+        countWhileInsert(TgTxOption.ofOCC());
+    }
+
+    @Test
+    void countWhileInserLtx() throws Exception {
+        countWhileInsert(TgTxOption.ofLTX());
+    }
+
+    @Test
+    void countWhileInserRtx() throws Exception {
+        countWhileInsert(TgTxOption.ofRTX());
+    }
+
+    private static final int COMMIT_SIZE = 100;
+
+    private void countWhileInsert(TgTxOption txOption) throws Exception {
+        var sql = "select count(*) from " + TEST;
+
+        var session = getSession();
+        var tm = session.createTransactionManager(TgTmSetting.ofAlways(txOption));
+        try (var ps = session.createQuery(sql, TgParameterMapping.of(), TgResultMapping.ofSingle(int.class))) {
+            var insertThread = new InsertThread();
+            insertThread.start();
+
+            Throwable occurred = null;
+            try {
+                while (!insertThread.end) {
+                    tm.execute(transaction -> {
+                        int count = transaction.executeAndFindRecord(ps, null).orElse(0);
+                        if (count % COMMIT_SIZE != 0) {
+                            fail(MessageFormat.format("count={0} (COMMIT_SIZE={1})", count, COMMIT_SIZE));
+                        }
+                    });
+                }
+            } catch (Throwable e) {
+                occurred = e;
+                throw e;
+            } finally {
+                insertThread.join();
+
+                var e = insertThread.exception;
+                if (e != null) {
+                    if (occurred != null) {
+                        occurred.addSuppressed(e);
+                    } else {
+                        if (e instanceof Exception) {
+                            throw (Exception) e;
+                        }
+                        throw new Exception(e);
+                    }
+                }
+            }
+        }
+    }
+
+    private static class InsertThread extends Thread {
+        private volatile boolean end = false;
+        private Throwable exception;
+
+        @Override
+        public void run() {
+            try {
+                execute();
+            } catch (Throwable e) {
+                this.exception = e;
+            } finally {
+                this.end = true;
+            }
+        }
+
+        private void execute() throws Exception {
+            try (var session = DbTestConnector.createSession()) {
+                var tm = session.createTransactionManager(TgTxOption.ofOCC());
+                try (var ps = session.createStatement(INSERT_SQL, INSERT_MAPPING)) {
+                    var foo = new AtomicInteger(0);
+                    for (int i = 0; i < 20; i++) {
+                        tm.execute((TsurugiTransactionAction) transaction -> {
+                            for (int j = 0; j < COMMIT_SIZE; j++) {
+                                var entity = createTestEntity(foo.getAndIncrement());
+                                transaction.executeAndGetCount(ps, entity);
+                            }
+                        });
+                    }
+                }
+            }
         }
     }
 }
