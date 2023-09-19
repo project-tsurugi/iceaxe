5887706326861f362c22c0aaeb2986d30e411d0e
Hishida Masato <hishida@nautilus-technologies.com>
2023-09-19 10:29:53 +0900

    DbInsertDuplicate2Test.mix(): Added retry count

--- modules/iceaxe-dbtest/src/test/java/com/tsurugidb/iceaxe/test/insert/DbInsertDuplicate2Test.java
+++ modules/iceaxe-dbtest/src/test/java/com/tsurugidb/iceaxe/test/insert/DbInsertDuplicate2Test.java
@@ -81,7 +81,7 @@ class DbInsertDuplicate2Test extends DbTestTableTester {
 
     @Test
     void mix() throws Exception {
-        var setting = TgTmSetting.of(TgTxOption.ofOCC(), 3, TgTxOption.ofLTX(TEST, TEST2), 1);
+        var setting = TgTmSetting.of(TgTxOption.ofOCC(), 3, TgTxOption.ofLTX(TEST, TEST2), 10);
         test(setting, 10, 500);
     }
 
