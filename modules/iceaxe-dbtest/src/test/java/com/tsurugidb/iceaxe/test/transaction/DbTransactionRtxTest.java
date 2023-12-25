51bca428d3eb80e9cfc1b9ee9f6b72528f2ae75c
Hishida Masato <hishida@nautilus-technologies.com>
2023-12-25 19:01:29 +0900

    test: Added DbTransactionRtxTest

---
+++ modules/iceaxe-dbtest/src/test/java/com/tsurugidb/iceaxe/test/transaction/DbTransactionRtxTest.java
@@ -0,0 +1,74 @@
+package com.tsurugidb.iceaxe.test.transaction;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.TestInfo;
+
+import com.tsurugidb.iceaxe.test.util.DbTestTableTester;
+import com.tsurugidb.iceaxe.transaction.TgCommitType;
+import com.tsurugidb.iceaxe.transaction.option.TgTxOption;
+
+/**
+ * transaction RTX test
+ */
+class DbTransactionRtxTest extends DbTestTableTester {
+
+    private static final int SIZE = 4;
+
+    @BeforeEach
+    void beforeEach(TestInfo info) throws Exception {
+        logInitStart(info);
+
+        dropTestTable();
+        createTestTable();
+        insertTestTable(SIZE);
+
+        logInitEnd(info);
+    }
+
+    @RepeatedTest(10)
+    void ltxUpdate_rtx() throws Exception {
+        var session = getSession();
+
+        try (var tx1 = session.createTransaction(TgTxOption.ofLTX(TEST))) {
+            try (var updatePs = session.createStatement("update " + TEST + " set bar = 9")) {
+                tx1.executeAndGetCount(updatePs);
+//              Thread.sleep(100);
+
+                try (var tx2 = session.createTransaction(TgTxOption.ofRTX())) {
+                    try (var selectPs = session.createQuery("select * from " + TEST)) {
+                        var list = tx2.executeAndGetList(selectPs);
+                        assertEquals(SIZE, list.size());
+                    }
+                    tx2.commit(TgCommitType.DEFAULT);
+                }
+            }
+
+            tx1.commit(TgCommitType.DEFAULT);
+        }
+    }
+
+    @RepeatedTest(10)
+    void ltxSelect_rtx() throws Exception {
+        var session = getSession();
+
+        try (var tx1 = session.createTransaction(TgTxOption.ofLTX())) {
+            try (var select1Ps = session.createQuery("select * from " + TEST + " where foo=1")) {
+                tx1.executeAndGetList(select1Ps);
+//              Thread.sleep(100);
+
+                try (var tx2 = session.createTransaction(TgTxOption.ofRTX())) {
+                    try (var selectPs = session.createQuery("select * from " + TEST)) {
+                        var list = tx2.executeAndGetList(selectPs);
+                        assertEquals(SIZE, list.size());
+                    }
+                    tx2.commit(TgCommitType.DEFAULT);
+                }
+            }
+
+            tx1.commit(TgCommitType.DEFAULT);
+        }
+    }
+}
