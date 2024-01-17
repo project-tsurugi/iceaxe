07f1f340157bc31ee2cb2002e4c98065168b5c41
Hishida Masato <hishida@nautilus-technologies.com>
2024-01-18 07:54:28 +0900

    test: DbInsertDuplicateTest.TIMEOUT 10min

--- modules/iceaxe-dbtest/src/test/java/com/tsurugidb/iceaxe/test/insert/DbInsertDuplicateTest.java
+++ modules/iceaxe-dbtest/src/test/java/com/tsurugidb/iceaxe/test/insert/DbInsertDuplicateTest.java
@@ -43,7 +43,7 @@ import com.tsurugidb.iceaxe.transaction.option.TgTxOption;
 class DbInsertDuplicateTest extends DbTestTableTester {
 
     private static final String TEST2 = "test2";
-    private static final long TIMEOUT = TimeUnit.MINUTES.toMillis(8);
+    private static final long TIMEOUT = TimeUnit.MINUTES.toMillis(10);
 
     @BeforeEach
     void beforeEach(TestInfo info) throws Exception {
