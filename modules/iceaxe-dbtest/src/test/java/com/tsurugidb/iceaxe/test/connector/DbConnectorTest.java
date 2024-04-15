a19478131cb62df55f68006fbb0dafb9adc85c5c
Hishida Masato <hishida@nautilus-technologies.com>
2024-04-15 12:42:44 +0900

    test: DbConnectorTest: Added applicationName()

--- modules/iceaxe-dbtest/src/test/java/com/tsurugidb/iceaxe/test/connector/DbConnectorTest.java
+++ modules/iceaxe-dbtest/src/test/java/com/tsurugidb/iceaxe/test/connector/DbConnectorTest.java
@@ -2,6 +2,7 @@ package com.tsurugidb.iceaxe.test.connector;
 
 import org.junit.jupiter.api.Test;
 
+import com.tsurugidb.iceaxe.session.TgSessionOption;
 import com.tsurugidb.iceaxe.test.util.DbTestConnector;
 import com.tsurugidb.iceaxe.test.util.DbTestTableTester;
 
@@ -11,12 +12,32 @@ import com.tsurugidb.iceaxe.test.util.DbTestTableTester;
 class DbConnectorTest extends DbTestTableTester {
 
     @Test
+    void label() throws Exception {
+        var sessionOption = TgSessionOption.of().setLabel("test-label");
+
+        var connector = DbTestConnector.createConnector();
+        try (var session = connector.createSession(sessionOption)) {
+            // TODO sessionからlabelを取得して確認したい
+        }
+    }
+
+    @Test
+    void applicationName() throws Exception {
+        var sessionOption = TgSessionOption.of().setApplicationName("test-app");
+
+        var connector = DbTestConnector.createConnector();
+        try (var session = connector.createSession(sessionOption)) {
+            // TODO sessionからapplicationNameを取得して確認したい
+        }
+    }
+
+    @Test
     void doNothing() throws Exception {
         try (var socket = DbTestConnector.createSocket()) {
             // do nothing
         }
 
-        // tateyama-serverが落ちていなければ、正常に動作する
+        // tsurugidbプロセスが落ちていなければ、正常に動作する
         var session = getSession();
         session.findTableMetadata(TEST);
     }
