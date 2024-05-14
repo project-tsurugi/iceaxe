6def208e95f7936b97191890a3fcb9aa5fe05794
Hishida Masato <hishida@nautilus-technologies.com>
2024-05-14 11:45:28 +0900

    test: TestLowSession: remove deprecated method

--- modules/iceaxe-core/src/test/java/com/tsurugidb/iceaxe/test/low/TestLowSession.java
+++ modules/iceaxe-core/src/test/java/com/tsurugidb/iceaxe/test/low/TestLowSession.java
@@ -1,27 +1,15 @@
 package com.tsurugidb.iceaxe.test.low;
 
 import java.io.IOException;
-import java.nio.ByteBuffer;
 import java.util.concurrent.TimeUnit;
 
 import com.tsurugidb.tsubakuro.channel.common.connection.Credential;
-import com.tsurugidb.tsubakuro.channel.common.connection.wire.ResponseProcessor;
 import com.tsurugidb.tsubakuro.channel.common.connection.wire.Wire;
 import com.tsurugidb.tsubakuro.common.Session;
 import com.tsurugidb.tsubakuro.util.FutureResponse;
 import com.tsurugidb.tsubakuro.util.ServerResource;
 
 public class TestLowSession extends TestServerResource implements Session {
-
-    @Override
-    public <R> FutureResponse<R> send(int serviceId, byte[] payload, ResponseProcessor<R> processor, boolean background) throws IOException {
-        throw new AssertionError("do override");
-    }
-
-    @Override
-    public <R> FutureResponse<R> send(int serviceId, ByteBuffer payload, ResponseProcessor<R> processor, boolean background) throws IOException {
-        throw new AssertionError("do override");
-    }
 
     @Override
     public FutureResponse<Void> updateCredential(Credential credential) throws IOException {
