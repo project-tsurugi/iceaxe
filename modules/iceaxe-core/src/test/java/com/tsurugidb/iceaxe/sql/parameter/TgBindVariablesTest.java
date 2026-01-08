0ab3aa7fe0ae8c6c378b211071f9f260864433c0
Hishida Masato <hishida@nautilus-technologies.com>
2026-01-08 17:28:08 +0900

    docs: update license header

--- modules/iceaxe-core/src/test/java/com/tsurugidb/iceaxe/sql/parameter/TgBindVariablesTest.java
+++ modules/iceaxe-core/src/test/java/com/tsurugidb/iceaxe/sql/parameter/TgBindVariablesTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023-2025 Project Tsurugi.
+ * Copyright 2023-2026 Project Tsurugi.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
